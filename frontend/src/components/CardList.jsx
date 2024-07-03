820ea1568d0e89344fd2226b039393c10801d53d
RafikSama <rafik23r@gmail.com>
2024-07-03 20:37:12 +0200

    dynamic route

--- frontend/src/components/CardList.jsx
+++ frontend/src/components/CardList.jsx
@@ -7,11 +7,11 @@ export default async function CardList() {
   return ( 
             <div className="grid gap-14 md:grid-cols-4 md:gap-4 mx-12">
                 {
-                    res.data.map((product , index)=>{
+                    res.data.map((product)=>{
                         return (
                             <div className="flex flex-col items-center" key={product._id}>
                                 <div className="w-60 h-40 bg-gray-100">
-                                    <img className="w-60 h-32 object-contain" src={product.image} alt="HAC-HFW2509TU-A-LED.png"/>
+                                    <Link href={`/products/${product._id}`}><img className="w-60 h-32 object-contain" src={product.image} alt="HAC-HFW2509TU-A-LED.png"/></Link>
                                 </div>
                                 <div className="px-6 py-4 text-center">
                                     <h1 className="font-bold text-xl text-[#001F3F] pb-4">{product.name}</h1>
